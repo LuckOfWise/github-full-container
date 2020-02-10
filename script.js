2d5a7d4bf1229d18db01bbedf5ad54fcf0fc28b2
LuckOfWise <luck.of.wise@gmail.com>
2020-02-10 17:10:14 +0900

    新デザイン対応

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request == "ONLY_LIVE") {
-		$('.room-participations .participation.online').each(function(){      
+		$('.room-large-participations .participation.online').each(function(){      
       if ($(this).data('icon') && $(this).data('icon').length > 0 && $(this).data('icon').indexOf('https://world-data-tokyo.s3.amazonaws.com/uploads/user/image/') == 0) {
         $(this).css('display', 'none');
       }
     });
   }
   if (request == 'SHOW_ALL') {
-		$('.room-participations .participation.online').css('display', 'inline-block');
+		$('.room-large-participations .participation.online').css('display', 'inline-block');
 	}
 });
